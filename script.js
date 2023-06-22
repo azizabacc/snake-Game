a7a7e9986d4f5e15f09905e13b885cdecdf3e7b3
Aziza Baccouri <azizabaccouri@gmail.com>
2023-06-22 21:04:07 +0200

    snake growth afer eating, still to fix its death when it heats the wall

--- script.js
+++ script.js
@@ -1,84 +1,91 @@
 let gameFloor = document.getElementById('gameFloor');
 let width = 59;
-let snakeIndex =0;
-let snakeLength =0;
-let mouseIndex =0;
-let direction =0;
 
-//generate Grid
-const generateGrid = () =>{
-    for(i=0;i<2000;i++){
-        let tile = document.createElement('div');
-        tile.className ="tile";
-        gameFloor.append(tile);
-    }
-}
-generateGrid();//call function
-//grid dimensions
-const getGridDimensions = () => {
-  const computedStyle = window.getComputedStyle(gameFloor);
-  const gridColumnTemplate = computedStyle.gridTemplateColumns;
-  const gridRowTemplate = computedStyle.gridTemplateRows;
-
-  const columnCount = gridColumnTemplate.split(' ').length;
-  const rowCount = gridRowTemplate.split(' ').length;
-
-  return {
-    columns: columnCount,
-    rows: rowCount
-  };
-};
-let dimensions = getGridDimensions();
-console.log(dimensions)
-//generate mouse
-const generateMouse = () =>{
-  do{
-    mouseIndex  = Math.floor(1 + Math.random() * gameFloor.childElementCount);
-    gameFloor.children[mouseIndex ].className="mouse";
-}while(snakeIndex==mouseIndex)
-}
-
-generateMouse();
-
-//switch Class function
-
-const switchClass = (snakeIndex,nextTileIndex)=>{ // SNAKE => SNAKEINDEX AND TILE => SNAKEINDEX+DIRECTION
-  if(gameFloor.children[nextTileIndex].className!='mouse' && gameFloor.children[nextTileIndex].className!='snake'){
-    gameFloor.children[snakeIndex].className ='tile';
-    gameFloor.children[nextTileIndex].className = 'snake';
-   
-  }else if(gameFloor.children[nextTileIndex].className=='mouse'){
-    snakeLength +=1;
-    gameFloor.children[nextTileIndex].className = 'snake';
-    snakeIndex = nextTileIndex;
-    generateMouse();
-
+// Class Snake
+class Snake {
+  constructor() {
+    this.snakeSegments = []; // array of snake's organs : le tail at the beginging and the head at the end
+    this.mouseIndex = 0;
+    this.direction = 0;
   }
 
-}
-const moveSnake = () => {
+  generateGrid() {
+    for (let i = 0; i < 2000; i++) {
+      let tile = document.createElement('div');
+      tile.className = "tile";
+      gameFloor.append(tile);
+    }
+  }
 
-  switchClass(snakeIndex, snakeIndex + direction);
-  snakeIndex  = snakeIndex + direction;
-};
-// move function
-const control= (e)=> {
+  getGridDimensions() {
+    const computedStyle = window.getComputedStyle(gameFloor);
+    const gridColumnTemplate = computedStyle.gridTemplateColumns;
+    const gridRowTemplate = computedStyle.gridTemplateRows;
+
+    const columnCount = gridColumnTemplate.split(' ').length;
+    const rowCount = gridRowTemplate.split(' ').length;
+
+    return {
+      columns: columnCount,
+      rows: rowCount
+    };
+  }
+
+  generateMouse() {
+    let mouseIndex;
+    do {
+      mouseIndex = Math.floor(1 + Math.random() * gameFloor.childElementCount);
+    } while (this.snakeSegments.includes(mouseIndex));
+
+    gameFloor.children[mouseIndex].className = "mouse";
+    return mouseIndex;
+  }
+
+  switchClass(nextTileIndex) {
+    if (gameFloor.children[nextTileIndex].className != 'mouse' && gameFloor.children[nextTileIndex].className != 'snake') {
+      const tailIndex = this.snakeSegments.shift(); // delete the tail of the snake
+      gameFloor.children[tailIndex].className = 'tile';
+
+      this.snakeSegments.push(nextTileIndex); // add the head of the snake
+      gameFloor.children[nextTileIndex].className = 'snake';
+    } else if (gameFloor.children[nextTileIndex].className == 'mouse') {
+      this.snakeSegments.push(nextTileIndex); // add a new head to the snake after eating
+      gameFloor.children[nextTileIndex].className = 'snake';
+
+      const newMouseIndex = this.generateMouse();
+      this.mouseIndex = newMouseIndex;
+    }
+  }
+
+  moveSnake() {
+    const headIndex = this.snakeSegments[this.snakeSegments.length - 1];
+    const nextTileIndex = headIndex + this.direction;
+    this.switchClass(nextTileIndex);
+  }
+
+  control(e) {
     if (e.keyCode === 39) {
-      direction = 1; // right
+      this.direction = 1; // right
     } else if (e.keyCode === 38) {
-      direction = -dimensions.columns; //up
+      this.direction = -this.getGridDimensions().columns; //up
     } else if (e.keyCode === 37) {
-      direction = -1; // left
+      this.direction = -1; // left
     } else if (e.keyCode === 40) {
-      direction = +dimensions.columns; // down 
-
+      this.direction = +this.getGridDimensions().columns; // down 
     }
-    
   }
-//generate Snake
-snakeIndex = Math.floor(1 + Math.random() * gameFloor.childElementCount);
-gameFloor.children[snakeIndex ].className="snake";
-setInterval(moveSnake,500);
 
-document.addEventListener("keydown",control)
+  startGame() {
+    const initialSnakeIndex = Math.floor(1 + Math.random() * gameFloor.childElementCount);
+    this.snakeSegments.push(initialSnakeIndex);
+    gameFloor.children[initialSnakeIndex].className = "snake";
+    setInterval(this.moveSnake.bind(this), 500);
+    document.addEventListener("keydown", this.control.bind(this));
+    this.generateMouse();
+  }
+}
 
+// create a new instance of the class snake
+const snakeGame = new Snake();
+snakeGame.generateGrid();
+snakeGame.startGame();
